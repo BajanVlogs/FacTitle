093024632376f988170c29360dccbdd38afcb130
BajanVlogs <kjolly544@gmail.com>
2017-10-20 00:49:18 -0400

    Update Task1.php

--- src/BajanVlogs/FacTitle/tasks/Task1.php
+++ src/BajanVlogs/FacTitle/tasks/Task1.php
@@ -1,1 +1,59 @@
+<?php
 
+
+namespace BajanVlogs\FacTitle\tasks;
+
+
+
+use pocketmine\Server;
+
+
+use pocketmine\schedulerPluginTask;
+
+
+use pocketmine\Player;
+
+
+
+use NidPlays\FacTitle\Main;
+
+
+
+
+
+
+
+class Task1 extends PluginTask {
+
+
+
+
+   public function __construct(Main $main) {
+
+
+        parent::__construct($main);
+
+
+        $this->main = $main;
+
+
+        $this->server = $main->getServer();
+
+
+    }
+
+
+
+
+   public function onRun($tick) {
+
+
+        $this->main->getLogger()->debug('Task ' . get_class($this) . ' is running on $tick'); 
+
+
+    }
+
+
+
+
+}
