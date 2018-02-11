03355a9c6921db33242999b0f7f19cb388d2bc4b
BajanVlogs <kjolly544@gmail.com>
2018-02-10 21:43:12 -0500

    Update Dummy1.php

--- src/BajanVlogs/FacTitle/Dummy1.php
+++ src/BajanVlogs/FacTitle/Dummy1.php
@@ -1,42 +1,18 @@
 <?php
 
-
 namespace BajanVlogs\FacTitle;
 
-
-
 use pocketmine\Server;
-
-
 use pocketmine\Player;
-
-
-
 use BajanVlogs\FacTitle\Main;
 
-
-
-
-
-
-
-class Dummy {
-
-
+class Dummy1 {
 
 
    public function __construct(Main $main) {
 
 
         $this->main = $main;
-
-
-        $this->server = $main->getServer()
-
-
-    }
-
-
-
-
-}
+        $this->server = $main->getServer();
+     }
+  }
