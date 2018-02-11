14f2810c147aca8fd2e406b8c8a2f3b6c9986fe6
BajanVlogs <kjolly544@gmail.com>
2018-02-10 21:44:43 -0500

    Update Task1.php

--- src/BajanVlogs/FacTitle/tasks/Task1.php
+++ src/BajanVlogs/FacTitle/tasks/Task1.php
@@ -1,19 +1,12 @@
 <?php
 
-
 namespace BajanVlogs\FacTitle\tasks;
-
-
 
 use pocketmine\Server;
 
-
 use pocketmine\schedulerPluginTask;
 
-
 use pocketmine\Player;
-
-
 
 use BajanVlogs\FacTitle\Main;
 
@@ -45,7 +38,7 @@ class Task1 extends PluginTask {
 
 
 
-   public function onRun($tick) {
+   public function onRun(int $tick) {
 
 
         $this->main->getLogger()->debug('Task ' . get_class($this) . ' is running on $tick'); 
